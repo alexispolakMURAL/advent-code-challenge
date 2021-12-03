672c37406d7bdf604a778d0acea44fca42b55783
Alexis Polak <alexisp@mural.co>
2021-12-03 17:45:34 -0300

    Added the day 3 challenges

--- day3/challenge1.js
+++ day3/challenge1.js
@@ -1,3 +1,20 @@
 // Day 3 - #1
 import { data } from "./data";
-const dataArray = data.split(/\r?\n/);
+const dataArray = data.split(/\r?\n/).map(m => m.split(''));
+
+let gammaRate = '';        // most common per column
+let epsilonRate = '';      // leat common per column
+
+const dataArray = data.split(/\r?\n/).map(m => m.split(''));
+
+for (let x = 0; x < dataArray[0].length; x++) {
+    let counter = 0;
+    for (let y = 0; y < dataArray.length; y++) {
+        counter += dataArray[y][x] > 0 ? 1 : -1;
+    }
+    gammaRate += counter > 0 ? '1' : '0';
+    epsilonRate += counter > 0 ? '0' : '1';
+}
+
+const powerConsumption = parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+console.log(powerConsumption);
--- day3/challenge2.js
+++ day3/challenge2.js
@@ -1,3 +1,40 @@
 // Day 3 - #2
 import { data } from "./data";
-const dataArray = data.split(/\r?\n/);
+
+const calculateCounter = (array, bitIndex) => { 
+    let ones = [];
+    let zero = [];
+    let counter = 0;
+
+    for (let y = 0; y < array.length; y++) {
+      counter += array[y][bitIndex] > 0 ? 1 : -1;
+  
+      if(array[y][bitIndex] > 0) {
+        counter += 1;
+        ones.push(array[y]);
+      } else {
+        counter -= 1;
+        zero.push(array[y]);
+      }
+    }
+    
+    return { ones, zero, counter };
+  };
+  
+  const calculate = (type, array, bitIndex = 0) => {
+      if(array.length === 1)
+        return array[0].join(''); 
+  
+      const counterData = calculateCounter(array, bitIndex);
+  
+      if(counterData.counter >= 0)
+          return calculate(type, type === 'o2' ? counterData.ones : counterData.zero, bitIndex+1);
+      else
+          return calculate(type, type === 'o2' ? counterData.zero : counterData.ones , bitIndex+1);
+  }
+  
+  const dataArray2 = data.split(/\r?\n/).map(m => m.split(''));
+  const oxygen = calculate('o2', dataArray2);
+  const co2 = calculate('co2', dataArray2);
+  
+  console.log(parseInt(oxygen, 2) * parseInt(co2, 2));
